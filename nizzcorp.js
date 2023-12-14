1652c0a92646edf6cbda1ae271d874cd43a345bc
rijo <rijorajan27594@gamil.com>
2023-12-14 08:33:53 +0530

    code formating

--- nizzcorp.js
+++ nizzcorp.js
@@ -1,61 +1,38 @@
-
-
 function gridStructure(row, column) {
-    
-    const gridContainer = document.getElementById("container")
+    const gridContainer = document.getElementById("container");
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
-            const gridElement = document.createElement('div')
-            gridElement.id=`item_${i}_${j}`;
+            const gridElement = document.createElement("div");
+            gridElement.id = `item_${i}_${j}`;
             gridElement.className = "gridelement";
-            
+
             gridContainer.appendChild(gridElement);
         }
     }
-    
-    const Button=document.createElement("button")
-    Button.textContent="Drag it"
-    Button.id="buttonn"
-    Button.setAttribute('draggable', true);
-    Button.addEventListener("dragstart",(e)=>{
-       
-        e.dataTransfer.setData("id",e.target.id)
-       
-    })
-    gridContainer.querySelector('#item_0_0').appendChild(Button)
-  
+
+    const Button = document.createElement("button");
+    Button.textContent = "Drag it";
+    Button.id = "buttonn";
+    Button.setAttribute("draggable", true);
+    Button.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("id", e.target.id);
+    });
+    gridContainer.querySelector("#item_0_0").appendChild(Button);
 }
-gridStructure(10, 10)
+gridStructure(10, 10);
 
+const dragContainer = document.getElementById("container");
+dragContainer.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
 
-const dragContainer=document.getElementById("container")
-dragContainer.addEventListener("dragover",(e)=>{
-  
-    e.preventDefault()
-  
-  
-})
-
-dragContainer.addEventListener("drop",(e)=>{
-
-     e.preventDefault()
-     const dragButonId=e.dataTransfer.getData("id")
-     const TheDragButton=document.getElementById(dragButonId)
-     const targett=e.target;
-     console.log("tarrrrrget",targett)
-     if(targett.className==="gridelement"){
-      targett.appendChild(TheDragButton)}
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
+dragContainer.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const dragButonId = e.dataTransfer.getData("id");
+    const TheDragButton = document.getElementById(dragButonId);
+    const targett = e.target;
+    console.log("tarrrrrget", targett);
+    if (targett.className === "gridelement") {
+        targett.appendChild(TheDragButton);
+    }
+});
